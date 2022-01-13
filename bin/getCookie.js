a58e3165df21fd13b8fe3e5ca5e705cdb36f1692
Kevin Cui <krazycavin@gmail.com>
2022-01-13 20:50:26 +0100

    Add missing puppeteer script

---
+++ bin/getCookie.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+process.removeAllListeners('warning');
+const puppeteer = require('puppeteer-core');
+const cPath = process.argv[2]; 
+const url = process.argv[3]; 
+const ua = process.argv[4]; 
+
+(async() => {
+  const browser = await puppeteer.launch({executablePath: cPath, headless: true});
+  const page = await browser.newPage();
+  await page.setUserAgent(ua);
+  await page.goto(url, {timeout: 15000, waitUntil: 'domcontentloaded'});
+  await page.waitForSelector("#btn:not([disabled])");
+  await page.click('#btn');
+  const cookie = await page.cookies();
+  console.log(JSON.stringify(cookie));
+  await browser.close();
+})();
